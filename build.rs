3bc6d0f809029c3c8f233f6dfee6b07f99da9470
Ivan Hrabcak <ivan@hrabcak.eu>
2024-09-09 22:49:33 +0200

    Only create an index file in subsequent builds

--- build.rs
+++ build.rs
@@ -10,22 +10,25 @@ fn main() {
             io::Write,
         };
 
-        // https://github.com/Aleph-Alpha/ts-rs/issues/133
-        let exports: Vec<_> = fs::read_dir("./bindings")
-            .unwrap()
-            .filter_map(Result::ok)
-            .filter_map(|p| {
-                p.path()
-                    .file_stem()
-                    .map(OsStr::to_str)
-                    .flatten()
-                    .map(str::to_owned)
-            })
-            .filter(|f| f != "index")
-            .map(|f| format!("export * from \"./{}\"", f))
-            .collect();
+        let result = fs::read_dir("./bindings");
 
-        let mut file = File::create("./bindings/index.ts").unwrap();
-        file.write_all(exports.join("\n").as_bytes()).unwrap();
+        if let Ok(bindings_dir) = result {
+            // https://github.com/Aleph-Alpha/ts-rs/issues/133
+            let exports: Vec<_> = bindings_dir
+                .filter_map(Result::ok)
+                .filter_map(|p| {
+                    p.path()
+                        .file_stem()
+                        .map(OsStr::to_str)
+                        .flatten()
+                        .map(str::to_owned)
+                })
+                .filter(|f| f != "index")
+                .map(|f| format!("export * from \"./{}\"", f))
+                .collect();
+    
+            let mut file = File::create("./bindings/index.ts").unwrap();
+            file.write_all(exports.join("\n").as_bytes()).unwrap();
+        }
     }
 }
