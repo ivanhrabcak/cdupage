f9e71516061a0f21de5a834fc3503abc8415232e
Matus Mastena <shadiness9530@proton.me>
2025-03-02 09:33:22 +0100

    Cargo fmt is better than GH actions

---
+++ ffi-sys/src/lib.rs
@@ -0,0 +1,16 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+use autocxx::prelude::*;
+include_cpp! {
+    #include "binds.hpp"
+    safety!(unsafe_ffi)
+    generate!("vector")
+    generate!("map")
+    generate!("string")
+}
+
+include!(concat!(
+    env!("_DIR"),
+    "/autocxx-build-dir/rs/autocxx-ffi-default-gen.rs"
+));
